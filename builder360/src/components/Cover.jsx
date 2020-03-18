d52d6911473b75b18ea571eabf023885d11cb620
Kusum Maharjan <kusu0011@edu.easj.dk>
2020-03-18 22:26:20 +0100

    Update Cover.jsx

--- builder360/src/components/Cover.jsx
+++ builder360/src/components/Cover.jsx
@@ -1,37 +1,29 @@
 import React, { Component } from "react";
-import { Slide , Zoom
- } from 'react-slideshow-image';
+import { Slide, Zoom } from "react-slideshow-image";
 import "./cover.css";
 
-
-const images = [
-    'assets/cover1.jpg',
-    'assets/cover2.jpg',
-    'assets/cover3.jpg'
-]
+const images = ["assets/cover1.jpg", "assets/cover2.jpg", "assets/cover3.jpg"];
 const properties = {
-    duration: 5000,
-    transitionDuration: 500,
-    infinite: true,
-    indicators: true,
-    arrows: true,
-    scale: 0.4,
-    pauseOnHover: true,
-    onChange: (oldIndex, newIndex) => {
-      console.log(`slide transition from ${oldIndex} to ${newIndex}`);
-    }
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true,
+  scale: 0.4,
+  pauseOnHover: true,
+  onChange: (oldIndex, newIndex) => {
+    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
   }
+};
 
 export default class cover extends Component {
   render() {
     return (
-        <div className="slide-container">
-             
-       
+      <div className="slide-container">
         <Zoom {...properties}>
-          {
-            images.map((each, index) => <img key={index} style={{width: "100%"}} src={each} />)
-          }
+          {images.map((each, index) => (
+            <img key={index} style={{ width: "100%" }} src={each} />
+          ))}
         </Zoom>
 
         {/* <div className="each-slide">
@@ -46,8 +38,7 @@ export default class cover extends Component {
           </div>
           <div className="each-slide">
             <div style={{'backgroundImage': `url(${slideImages[2]})`}}> */}
-            </div>
-        
-      
-    )}
+      </div>
+    );
+  }
 }
