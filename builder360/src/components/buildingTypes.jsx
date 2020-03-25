0e9162f8d6bf680f8e7ba42d1a2139d0818c4dcb
Kusum Maharjan <kusu0011@edu.easj.dk>
2020-03-25 14:36:00 +0100

    font and all

--- builder360/src/components/buildingTypes.jsx
+++ builder360/src/components/buildingTypes.jsx
@@ -8,85 +8,107 @@ export default class buildingTypes extends Component {
   render() {
     return (
       <main>
-      
-          {/* Building Types Section */}
-          <Tab.Container id="building" defaultActiveKey="first">
-            <h2 className="customHead">Building Types</h2>
+        {/* Building Types Section */}
+        <Tab.Container id="building" defaultActiveKey="first">
+          <h2 className="customHead">Building Types</h2>
 
-            <Row className="buildingtabs">
-              <Col sm={6} className="image-col">
-                <Nav variant="pills">
-                  <Nav.Link eventKey="first">
-                    <img class="img-fluid" height="200" width="200" src="assets/typeA.png" alt="" />
-                  </Nav.Link>
-                  <Nav.Link eventKey="second">
-                    <img class="img-fluid" height="200" width="200" src="assets/typeB.jpg" alt="" />
-                  </Nav.Link>
+          <Row className="buildingtabs">
+            <Col md={6} className="image-col">
+              <Nav variant="pills">
+                <Nav.Link eventKey="first">
+                  <img
+                    class="img-fluid"
+                    height="200"
+                    width="200"
+                    src="assets/typeA.png"
+                    alt=""
+                  />
+                </Nav.Link>
+                <Nav.Link eventKey="second">
+                  <img
+                    class="img-fluid"
+                    height="200"
+                    width="200"
+                    src="assets/typeB.jpg"
+                    alt=""
+                  />
+                </Nav.Link>
 
-                  <Nav.Link eventKey="third">
-                    <img class="img-fluid" height="200" width="200" src="assets/typeC.jpg" alt="" />
-                  </Nav.Link>
+                <Nav.Link eventKey="third">
+                  <img
+                    class="img-fluid"
+                    height="200"
+                    width="200"
+                    src="assets/typeC.jpg"
+                    alt=""
+                  />
+                </Nav.Link>
 
-                  <Nav.Link eventKey="fourth">
-                    <img class="img-fluid" height="200" width="200" src="assets/typeD.jpg" alt="" />
-                  </Nav.Link>
-                </Nav>
-              </Col>
-              <Col sm={6}>
-                <Tab.Content>
-                  <h4 className="smallHead">
-                    SHORTEN THE DESIGN PROCESS AND CHOOSE ON OF OUR
-                    ARCHITECTURAL HOUSE TYPES.
-                  </h4>
+                <Nav.Link eventKey="fourth">
+                  <img
+                    class="img-fluid"
+                    height="200"
+                    width="200"
+                    src="assets/typeD.jpg"
+                    alt=""
+                  />
+                </Nav.Link>
+              </Nav>
+            </Col>
+            <Col md={6}>
+              <Tab.Content>
+                <h4 className="smallHead">
+                  SHORTEN THE DESIGN PROCESS AND CHOOSE ON OF OUR ARCHITECTURAL
+                  HOUSE TYPES.
+                </h4>
 
-                  <Tab.Pane eventKey="first">
-                    <h5>
-                      <span>A.</span> The Town House
-                    </h5>
-                    <p>
-                      The Town House is a very popular form of housing in
-                      Denmark.This is a well executed one and a half or two
-                      stories housing type, inspired by classic Danish housing
-                      architecture.
-                    </p>
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="second">
-                    <h5>
-                      <span>B.</span> The Compact House
-                    </h5>
-                    <p>
-                      Rectangular in shape, the Compact House has very beautiful
-                      features. The geometry of this kind of housing is very
-                      cost-effective, stylish and structural.
-                    </p>
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="third">
-                    <h5>
-                      <span>C.</span> The Angle House
-                    </h5>
-                    <p>
-                      The Angle House is a great choice for people who love to
-                      separate personal and social space. The bedrooms and
-                      living areas can be divided and yard space can be created
-                      in this housing type.
-                    </p>
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="fourth">
-                    <h5>
-                      <span>D.</span> The Funkis House
-                    </h5>
-                    <p>
-                      The Funkis House is quite similar in layout to the classic
-                      Nepalese homes. It has a flat roof and normally built in
-                      double stories.
-                    </p>
-                  </Tab.Pane>
-                </Tab.Content>
-              </Col>
-            </Row>
-          </Tab.Container>
-          {/* Buidling Types Section */}
-         
+                <Tab.Pane eventKey="first">
+                  <h5>
+                    <span>A.</span> The Town House
+                  </h5>
+                  <p>
+                    The Town House is a very popular form of housing in
+                    Denmark.This is a well executed one and a half or two
+                    stories housing type, inspired by classic Danish housing
+                    architecture.
+                  </p>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  <h5>
+                    <span>B.</span> The Compact House
+                  </h5>
+                  <p>
+                    Rectangular in shape, the Compact House has very beautiful
+                    features. The geometry of this kind of housing is very
+                    cost-effective, stylish and structural.
+                  </p>
+                </Tab.Pane>
+                <Tab.Pane eventKey="third">
+                  <h5>
+                    <span>C.</span> The Angle House
+                  </h5>
+                  <p>
+                    The Angle House is a great choice for people who love to
+                    separate personal and social space. The bedrooms and living
+                    areas can be divided and yard space can be created in this
+                    housing type.
+                  </p>
+                </Tab.Pane>
+                <Tab.Pane eventKey="fourth">
+                  <h5>
+                    <span>D.</span> The Funkis House
+                  </h5>
+                  <p>
+                    The Funkis House is quite similar in layout to the classic
+                    Nepalese homes. It has a flat roof and normally built in
+                    double stories.
+                  </p>
+                </Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+        {/* Buidling Types Section */}
       </main>
     );
   }
--- builder360/src/components/collab.jsx
+++ builder360/src/components/collab.jsx
@@ -9,43 +9,42 @@ export default class collab extends Component {
   render() {
     return (
       <main>
-          {/* Collab Section */}
-          <h2 className="customHead">Collaborate with us</h2>
+        {/* Collab Section */}
+        <h2 className="customHead">Collaborate with us</h2>
 
-          <Row className="collab">
-            <Col xs={6}>
-              <Image src="assets/collab.png"></Image>
-            </Col>
+        <Row className="collab">
+          <Col md={6}>
+            <Image src="assets/collab.png"></Image>
+          </Col>
 
-            <Col xs={6}>
-              <h5>Tell Us about yourself:</h5>
-              <Form>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                  <Form.Control type="email" placeholder="Name" />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                  <Form.Control type="email" placeholder="Email Address" />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                  <Form.Control type="email" placeholder="Address" />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                  <Form.Control type="email" placeholder="Phone Number" />
-                </Form.Group>
+          <Col md={6}>
+            <h4 className="collabHead">Tell Us about yourself:</h4>
+            <Form>
+              <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Control type="email" placeholder="Name" />
+              </Form.Group>
+              <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Control type="email" placeholder="Email Address" />
+              </Form.Group>
+              <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Control type="email" placeholder="Address" />
+              </Form.Group>
+              <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Control type="email" placeholder="Phone Number" />
+              </Form.Group>
 
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                  <Form.Control
-                    as="textarea"
-                    placeholder="Your Message Here"
-                    rows="5"
-                  />
-                </Form.Group>
-                <Button type="submit">Send</Button>
-              </Form>
-            </Col>
-          </Row>
-          {/* Collab Section */}
-     
+              <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Control
+                  as="textarea"
+                  placeholder="Your Message Here"
+                  rows="5"
+                />
+              </Form.Group>
+              <Button type="submit">Send</Button>
+            </Form>
+          </Col>
+        </Row>
+        {/* Collab Section */}
       </main>
     );
   }
